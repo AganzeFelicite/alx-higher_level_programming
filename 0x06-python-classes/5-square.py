65e769f06e509419db9d209107e403d33e525af9
AganzeFelicite <aganzefelicite@gmail.com>
2022-06-24 04:43:56 +0200

    a quick hot fix on this task

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -29,7 +29,10 @@ class Square:
         self.__size = value
     """this is to print the square object"""
     def my_print(self):
+        """print the square"""
         for i in range(self.__size):
             for j in range(self.__size):
                 print("#", end="")
-            print()
+            print("")
+        if self.__size = 0:
+            print("")
