7ad4e4ddbb2983d1d56f3367937ed3279b802980
AganzeFelicite <aganzefelicite@gmail.com>
2022-06-24 03:54:34 +0200

    this is a commit on the third square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" a class that defines a square"""
+
+
+class Square:
+    """Defines methods for the class Square"""
+    def __init__(self, size=0):
+        """check the conditions"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        """this is an instantion in square"""
+        self.__size = size
+    """to calculate the area of the class"""
+    def area(self):
+        return self.__size * self.__size
