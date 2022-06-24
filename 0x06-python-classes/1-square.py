d288300399579827a26283e7a930f496e3a2a34e
AganzeFelicite <aganzefelicite@gmail.com>
2022-06-24 03:29:21 +0200

    this is a new task on the class square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" a class that defines a square"""
+
+
+class Square:
+    """Defines methods for the class Square"""
+    def __init__(self, size: int):
+        """this is an instantion in square"""
+        self.__size = size
