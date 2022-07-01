97ba6517a171eb1089ae8d9cb4437a0468418827
AganzeFelicite <aganzefelicite@gmail.com>
2022-07-01 11:09:13 +0200

    task 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-""" 0-add_module 
-    to add 
-    two ints
-
+"""
+    0-add_module to add two ints
 """
 
 
-def add_integer(a, b= 89):
+def add_integer(a, b=89):
     """Return the addition of two numbers"""
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
