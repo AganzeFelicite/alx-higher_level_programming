fb6d82ade5445269b49c678c8f82e6b8efbe3650
AganzeFelicite <aganzefelicite@gmail.com>
2022-07-08 14:42:10 +0200

    this an improved basegeometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""this is the BaseGeometry module"""
+
+
+class BaseGeometry:
+    """ this is my class on base geometry"""
+    def area(self):
+        """this raises the error"""
+        raise Exception("area() is implemented")
