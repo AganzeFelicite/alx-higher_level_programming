a83fb12de1c5005698d296c14c2c8bbf63eb2ef8
AganzeFelicite <aganzefelicite@gmail.com>
2022-07-08 14:01:32 +0200

    task 3

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -3,8 +3,8 @@
 
 
 def is_kind_of_class(obj, a_class):
-    """ it is a function to check if a give 
-        object is from a specific class
+    """ it is a function to check if a give
+    object is from a specific class
     """
 
     return isinstance(obj, a_class)
