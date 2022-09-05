7d3aaea71541709fe3a4037067f42c07fcf038d9
AganzeFelicite <aganzefelicite@gmail.com>
2022-09-05 20:28:32 +0200

    javascript is awesome

---
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+/**
+ * this is to print javascript is amazing
+ */
+
+const myVar = 'JavaScript is amazing';
+console.log(myVar);
---
+++ 0x12-javascript-warm_up/README.md
@@ -0,0 +1,2 @@
+this is a quick warm up on javascript the language of the web
+
