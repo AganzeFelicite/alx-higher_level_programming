274c7a32761f59a5596c62f2e70a371619743b04
AganzeFelicite <aganzefelicite@gmail.com>
2022-09-05 20:43:50 +0200

    print threee lines

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/**
+ * to print tree lines
+ */
+const word = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < word.length; i++) {
+  console.log(word[i]);
+}
