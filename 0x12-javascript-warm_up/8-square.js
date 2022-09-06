ad48225acc4c4b1369fffdbc038e2fa96fa9fa87
AganzeFelicite <aganzefelicite@gmail.com>
2022-09-06 05:45:47 +0200

    this is a square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/**
+ * this is about a square
+ * */
+const str = 'x';
+const num = parseInt(process.argv[2], 10);
+if (num) {
+  for (let i = 1; i <= num; i++) {
+    console.log(str.repeat(num));
+  }
+} else {
+  console.log('Missing size');
+}
