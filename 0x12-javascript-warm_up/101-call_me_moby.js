d3bbe1441e894609b15c739fe7ef71f00d526443
AganzeFelicite <aganzefelicite@gmail.com>
2022-09-08 00:22:58 +0200

    a semistandard fix

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-/*this is a function that execute x timea
+/* this is a function that execute x timea
  */
 
 exports.callMeMoby = function (x, theFunction) {
   for (let i = 1; i <= x; i++) {
     theFunction();
   }
-}	
+};
