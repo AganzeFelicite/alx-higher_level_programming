e9b42edd3b96bff2ac7bf99188db97c6c23dd8c1
AganzeFelicite <aganzefelicite@gmail.com>
2022-09-07 23:26:27 +0200

    this a hot a fix on the second last

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -6,15 +6,9 @@
 if (process.argv[2] === undefined || process.argv[3] === undefined) {
   console.log(0);
 }
-let large = parseInt(process.argv[2]);
-let secondlarge = 0;
 const array = process.argv;
 for (let i = 2; i < array.length; i++) {
-  if (parseInt(array[i]) > large) {
-    secondlarge = large;
-    large = parseInt(array[i]);
-  } else if (parseInt(array[i]) > secondlarge && array.length > 3) {
-    secondlarge = parseInt(array[i]);
-  }
+  array[i] = parseInt(array[i]);
 }
-console.log(secondlarge);
+array.sort(function (a, b) { return b - a; });
+console.log(array[3]);
