5f35bb2515235243c3136161abb1504ad7b73936
AganzeFelicite <aganzefelicite@gmail.com>
2022-09-07 23:37:41 +0200

    still hot fix

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,12 +3,17 @@
  * a script that searches for the second biggest
  * integer in the list of arguments
  * */
-if (process.argv[2] === undefined || process.argv[3] === undefined) {
-  console.log(0);
+function secondlast () {
+  if (process.argv[2] === undefined || process.argv[3] === undefined) {
+    console.log(0);
+    return 0;
+  }
+  const array = process.argv;
+  for (let i = 2; i <= array.length - 1; i++) {
+    array[i] = parseInt(array[i]);
+  }
+  array.sort(function (a, b) { return b - a; });
+  console.log(array[3]);
 }
-const array = process.argv;
-for (let i = 2; i < array.length; i++) {
-  array[i] = parseInt(array[i]);
-}
-array.sort(function (a, b) { return b - a; });
-console.log(array[3]);
+
+secondlast();
