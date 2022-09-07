a94082d7278997a2a5fa922af6e4928120cc9653
AganzeFelicite <aganzefelicite@gmail.com>
2022-09-08 00:34:08 +0200

    a  function that increments and calls a function.

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-/*a function that increments and calls a function.
+/* a function that increments and calls a function.
  */
 
 exports.addMeMaybe = function (number, theFunction) {
   number++;
   theFunction(number);
-}
+};
