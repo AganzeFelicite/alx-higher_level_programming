9a16df1a27d2b79ae6d03858cdd09f5b9f53416f
AganzeFelicite <aganzefelicite@gmail.com>
2022-09-09 12:26:02 +0200

    this is character sets and collations

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,8 @@
+-- this a script to alter the table character set to utf-8
+
+USE hbtn_0c_0
+ALTER DATABASE hbtn_0c_0 
+CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table
+CONVERT TO  CHARACTER SET utf8mb4 
+COLLATE utf8mb4_unicode_ci;
