df54e688999cbf96236257344c9325dd665d3744
AganzeFelicite <aganzefelicite@gmail.com>
2022-09-12 17:43:04 +0200

    this is a hot fix

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,5 @@
 -- this is to show create the database and the second user
 CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
 
