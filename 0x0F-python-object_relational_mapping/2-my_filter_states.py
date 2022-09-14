eeb52b55078e95082909df06ef51911421b3209c
AganzeFelicite <aganzefelicite@gmail.com>
2022-09-14 17:25:44 +0200

    simple database search

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""this is a script to search for a user's input in the database"""
+import MySQLdb
+from sys import argv
+
+
+def search():
+    """
+    this is a function that creates
+    a database connection and do the
+    select of the argv input
+    """
+    hostname = "localhost"
+    User = argv[1]
+    password = argv[2]
+    database = argv[3]
+    word = argv[4]
+    con = MySQLdb.connect(
+        host=hostname,
+        user=User,
+        passwd=password, db=database, port=3306)
+    cur = con.cursor()
+    querry = """SELECT * FROM states WHERE name = '{:s}'
+            ORDER BY id ASC""".format(word)
+    cur.execute(querry)
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+
+if __name__ == "__main__":
+    search()
