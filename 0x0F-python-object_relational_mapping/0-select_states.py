0fe6282f9b31d5b5d3b9221de52679c1604090ff
AganzeFelicite <aganzefelicite@gmail.com>
2022-09-14 13:02:03 +0200

    hot fix

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,19 +2,23 @@
 import MySQLdb
 from sys import argv
 
+
 def main():
-    """this is to make a database connection 
+    """this is to make a database connection
     """
     User = argv[1]
     Passwrd = argv[2]
     DataBase = argv[3]
     hostname = "localhost"
-    con = MySQLdb.Connect(host = hostname, user = User, passwd = Passwrd, db = DataBase, port=3306);
+    con = MySQLdb.Connect(
+        host=hostname, user=User,
+        passwd=Passwrd, db=DataBase,
+        port=3306)
     db = con.cursor()
     db.execute("SELECT * FROM states ORDER BY id ASC")
+
     for row in db:
         print(row)
 
 
 main()
-
