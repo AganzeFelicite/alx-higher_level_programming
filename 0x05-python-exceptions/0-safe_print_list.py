582368a1319ac597bd74a94ef1c659f3bd2ec39b
AganzeFelicite <aganzefelicite@gmail.com>
2022-10-05 23:48:22 +0200

    new error fix

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,7 +8,7 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print(my_list[i], end="")
         print()
+        return i + 1
     except IndexError:
         print()
-    finally:
-        return i + 1
+        return i
