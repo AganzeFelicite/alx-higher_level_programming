8600e89a943c33ab5ce2dc32d167476e59e9682c
AganzeFelicite <aganzefelicite@gmail.com>
2022-10-05 23:59:41 +0200

    clean

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,19 +1,15 @@
 #!/usr/bin/python3
 """this print elements of a lists in anorder"""
 
-
-from http.client import CannotSendRequest
-
-
 def safe_print_list(my_list=[], x=0):
     '''this prints the list elements'''
-    counting = 0
+    val = 0
     try:
         for i in range(x):
             print(my_list[i], end="")
-            counting += 1
+            val += 1
         print()
-        return counting
+        return val
     except IndexError:
         print()
-        return counting
+        return val
