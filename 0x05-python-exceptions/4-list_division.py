47d37d48c5c357f05abb96346f75458adba65835
AganzeFelicite <aganzefelicite@gmail.com>
2022-10-06 16:14:46 +0200

    list division

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""This devides the elements of two array"""
+
+
+from sqlalchemy import null
+
+
+def list_division(my_list_1, my_list_2, list_length):
+
+    """this return a list of the divided values"""
+    new_list = []
+    try:
+        for i in range(list_length):
+            try:
+                result = my_list_1[i] / my_list_2[i]
+                new_list.append(result)
+            except(TypeError, ValueError,):
+                print("wrong type")
+                new_list.append(0)
+            except(ZeroDivisionError):
+                print("division by 0")
+                new_list.append(0)
+            except(IndexError):
+                print("out of range")
+                new_list.append(0)
+    finally:
+        print(new_list)
