875873b483aa8146d49bc3ce70399350572fb188
AganzeFelicite <aganzefelicite@gmail.com>
2022-11-29 14:49:54 +0200

    save, load , add

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """this load,add,and save to a json file"""
 from sys import argv
-save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
-load_from_json_file = __import__("6-load_from_json_file.py").load_from_json_file
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 
 args = argv
+filename = "add_item.json"
 if len(args) > 1:
     args = args[1:]
 else:
     args = []
-filename = "add_item.json"
 try:
     text = load_from_json_file(filename)
 except Exception:
