7c7f10ae8d18049a141f2ea26758e4e440900de1
AganzeFelicite <aganzefelicite@gmail.com>
2022-11-29 13:13:03 +0200

    read in python file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python3
 """A module that reads a file and prints it"""
 
@@ -7,5 +6,5 @@ def read_file(filename=""):
     """A function that reads a file and prints it"""
 
     if type(filename) is str:
-        with open(filename, mode="r", encoding="utf-8") as f_name:
-            print(f_name.read(), end="")
+        with open(filename, mode="r", encoding="utf-8") as f:
+            print(f.read(), end="")
