7151aa799068dd009192e4fea3d712871bb75b2e
AganzeFelicite <aganzefelicite@gmail.com>
2023-03-31 15:20:01 +0200

    header response treatment"

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+this is a script that takes in a response to a
+url and print the value of a key in the header
+response
+"""
+
+
+if __name__ == "__main__":
+    import urllib.request
+    import sys
+
+    url = sys.argv[1]
+    url_req = urllib.request.Request(url)
+    with urllib.request.urlopen(url_req) as resp:
+        print(dict(resp.headers)["X-Request-Id"])
