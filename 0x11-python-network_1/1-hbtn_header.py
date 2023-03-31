10e14bff33284e3b0f86f5d6f5c06c3271116ae1
AganzeFelicite <aganzefelicite@gmail.com>
2023-03-31 15:27:58 +0200

    new fixes:

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,8 +4,6 @@ this is a script that takes in a response to a
 url and print the value of a key in the header
 response
 """
-
-
 if __name__ == "__main__":
     import urllib.request
     import sys
@@ -13,4 +11,4 @@ if __name__ == "__main__":
     url = sys.argv[1]
     url_req = urllib.request.Request(url)
     with urllib.request.urlopen(url_req) as resp:
-        print(dict(resp.headers)["X-Request-Id"])
+        print(dict(resp.headers).get("X-Request-Id"))
