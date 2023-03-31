c4a295dcf2b4a5795d19bd011d19883459a68817
AganzeFelicite <aganzefelicite@gmail.com>
2023-03-31 14:30:52 +0200

    pycode style passed

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 """
 this is a python script that
-fetch content of the 
-the url provide in the code
+fetch content of the he url provide in the code
 and displays it in the format
 shown
 """
 if __name__ == "__main__":
     import urllib.request
 
-    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
-        data = response.read()
+    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as res:
+        data = res.read()
         print("Body response: $")
         print("\t- type: {}".format(type(data)))
         print("\t- content: {}".format(data))
