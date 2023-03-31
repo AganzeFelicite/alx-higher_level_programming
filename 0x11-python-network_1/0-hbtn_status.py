37205222e6de74720d641606b887e6085a679667
AganzeFelicite <aganzefelicite@gmail.com>
2023-03-31 14:58:35 +0200

    from utf-8 to utf8

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -15,4 +15,4 @@ if __name__ == "__main__":
         print("Body response:")
         print("\t- type: {}".format(type(data)))
         print("\t- content: {}".format(data))
-        print("\t- utf-8 content: {}".format(data.decode("utf-8")))
+        print("\t- utf8 content: {}".format(data.decode("utf-8")))
