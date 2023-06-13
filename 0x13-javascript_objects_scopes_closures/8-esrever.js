e46eda3031dcb79c40ae6081989176a34bae4400
AganzeFelicite <aganzefelicite@gmail.com>
2023-06-13 20:43:30 +0200

    bug fix

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -5,7 +5,7 @@
 
 exports.esrever = function (list) {
   const arr = [];
-  for (let i = list.length; i >= 0; i--) {
+  for (let i = list.length - 1; i >= 0; i--) {
     arr.push(list[i]);
   }
   return arr;
