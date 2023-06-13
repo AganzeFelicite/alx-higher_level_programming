8698269908a7df4e7ef5b3c0aad69282071ebf6a
AganzeFelicite <aganzefelicite@gmail.com>
2023-06-13 14:25:39 +0200

    a bug fixed

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -13,7 +13,7 @@ class Rectangle {
 
   print () {
     for (let i = 0; i < this.height; i++) {
-      for (let j = 0; j < this.width; j++) { console.log('X'.repeat(this.width)); }
+      console.log('X'.repeat(this.width));
     }
   }
 }
