0e72242a957860dd9e054f00cf28f9f13ca45da0
AganzeFelicite <aganzefelicite@gmail.com>
2023-06-13 13:57:26 +0200

    defines the width and height of rectangle

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/**
+ * this si a rectangle class defined
+ */
+
+class Rectangle {
+  constructor (w, h) {
+    this.width = Number(w);
+    this.height = Number(h);
+  }
+}
+module.exports = Rectangle;
