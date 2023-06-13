cc997b10a6d2a03e4b2109efad34c03fd36df354
AganzeFelicite <aganzefelicite@gmail.com>
2023-06-13 15:06:51 +0200

    this semistard fixxer

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -2,7 +2,7 @@
 /**
  * this a square class that inherits from Rectangle of 4-rectangle.js
  * You must use the class notation for defining your class and extends
- * 
+ *
  */
 
 const Rectangle = require('./4-rectangle');
