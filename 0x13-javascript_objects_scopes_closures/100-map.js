7d49945d1085675fa88fbc3ec7c73783d8b85b17
AganzeFelicite <aganzefelicite@gmail.com>
2023-06-18 05:19:06 +0200

    the import modified

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -3,6 +3,6 @@
  * this multiplies each item in the list by its index:x
  *
  */
-const list = require('./101-data.js').list;
+const list = require('./100-data.js').list;
 console.log(list);
 console.log(list.map((item, index) => item * index));
