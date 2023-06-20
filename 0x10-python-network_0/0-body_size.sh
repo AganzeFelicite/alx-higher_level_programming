6d8a2dffb5076856c0e4749f0ead48511fa08350
AganzeFelicite <aganzefelicite@gmail.com>
2023-06-20 16:23:22 +0200

    initial commit

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+#this is a bash script to print the curl size, on a host
+# Display size of body of response; Usage: ./0-body_size.sh 0.0.0.0:5000
+curl -sI '$1' |grep 'Content-Length:' |cut -f2 -d' '
---
+++ 0x10-python-network_0/README.md
@@ -0,0 +1,2 @@
+python Networking "CURL COMMAND"
+
