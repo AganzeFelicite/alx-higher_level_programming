bc83c94bc4b6114d44f2840c930a90c065b82bba
AganzeFelicite <aganzefelicite@gmail.com>
2023-06-20 20:27:50 +0200

    some changes

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script takes in a URL, sends a GET request to the URL, and displays the methods present 
-curl -sI "$1" | grep 'Allow:' | cut -d' ' -f2- -d' '
+curl -sI "$1" | grep 'Allow:' | cut  -f2- -d' '
