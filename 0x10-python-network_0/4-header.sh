c44b2e9b9e469c3d0c4829fd339a6cd30e5dae74
AganzeFelicite <aganzefelicite@gmail.com>
2023-06-20 20:38:46 +0200

    fixed

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#this script script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
-curl -sH "X-School-User-Id:" 98 "$1"
+#this script script that takes in a URL as an argument, sends a GET request to the URL
+curl -sH "X-School-User-Id": 98 "$1"
